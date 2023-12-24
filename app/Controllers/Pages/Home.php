7bed7d4d806845ee4cdb04b6facdf2ef1c229175
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-12-24 14:32:32 -0300

    Fix: description

--- app/Controllers/Pages/Home.php
+++ app/Controllers/Pages/Home.php
@@ -3,16 +3,18 @@
 namespace App\Controllers\Pages;
 use \App\Utils\View;
 
-class Home {
+class Home extends Page{
   /**
    * Method that return home content
    * @return string 
   */
   public static function index() {
-    return View::render('/Pages/home', [
+    $content = View::render('/Pages/home', [
       'name' => 'Codetech Soluções em Software',
-      'description'=> 'Software and Consulting employment',
+      'description'=> 'Tornamos suas idéias de negócio em realidade',
       'site'=> 'https://www.codetechbr.net.br',
     ]);
+
+    return parent::indexPage('Codetech Soluções em Software', $content);
   }
 }
