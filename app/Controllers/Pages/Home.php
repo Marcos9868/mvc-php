e852776303754482f891f384f488f5d96f685ad3
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-12-24 13:33:16 -0300

    Feat: Home controller

---
+++ app/Controllers/Pages/Home.php
@@ -0,0 +1,13 @@
+<?php 
+
+namespace App\Controllers\Pages;
+
+class Home {
+  /**
+   * Method that return home content
+   * @return string 
+  */
+  public static function index() {
+    return 'Hello World';
+  }
+}
