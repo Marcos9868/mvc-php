474de8b8872fdc5a326bce8a338feb1d9ee0e3bc
Marcos9868 <marcos_m_ferreira@outlook.com>
2023-12-24 14:48:34 -0300

    Feat: Organization model

---
+++ app/Models/Entity/Organization.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace App\Models\Entity;
+
+class Organization {
+  public $id;
+  public $name = 'Codetech Soluções em Software';
+  public $website = 'https://www.codetechbr.net.br';
+  public $description = '
+    Uma empresa focada em fornecer serviços de fabricação
+    de software, sistemas e aplicativos, prestar consultoria
+    e análise estratégica de dados, além de fornecer suporte técnico
+    para cloud e hospedagens, transformando as necessidades
+    de nossos clientes em realidade.
+  ';
+}
